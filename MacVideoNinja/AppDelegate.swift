7f84aa876f330b55602c166c9b0858be9bddaed7
Steve Sheets <magesteve@mac.com>
2020-08-10 14:22:00 -0400

    Initial Commit

---
+++ MacVideoNinja/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacVideoNinja
+//
+//  Created by Steve Sheets on 8/10/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacVideoNinja/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacVideoNinja
+//
+//  Created by Steve Sheets on 8/10/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
